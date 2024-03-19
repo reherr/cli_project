fd1c34cf75c7b97ce6455e70d33b1bcbafe10bc7
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-03-19 12:59:56 +0000

    fixed code line 6 in test

--- tests/test_workout.rb
+++ tests/test_workout.rb
@@ -3,7 +3,7 @@ require './workout'
 
 class TestExercise < Minitest::Test
   def test_to_s
-    exercise = Exercise.new
-    assert_equal "dead lifts: 3 sets of 8 reps at 100 lbs.", exercise.to_s("dead lifts", 3, 8, 100), "to_s method failed"
+    exercise = Exercise.new("dead lifts", 3, 8, 100)
+    assert_equal "dead lifts: 3 sets of 8 reps at 100 lbs.", exercise.to_s, "to_s method failed"
   end
 end
