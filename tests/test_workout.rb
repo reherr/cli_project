283892b87701a64a73f71e32d9765955ef3c508d
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-03-19 12:57:01 +0000

    updated to_s test

--- tests/test_workout.rb
+++ tests/test_workout.rb
@@ -1,9 +1,9 @@
 require 'minitest/autorun'
 require './workout'
 
-class TestExercise < Minitest :: Test
+class TestExercise < Minitest::Test
   def test_to_s
     exercise = Exercise.new
-    assert_equal "dead lifts: 3 sets of 8 reps at 100 lbs.", workout.to_s(dead lifts, 3, 8, 100), "to_s method failed"
+    assert_equal "dead lifts: 3 sets of 8 reps at 100 lbs.", exercise.to_s("dead lifts", 3, 8, 100), "to_s method failed"
   end
 end
